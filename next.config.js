0328ef9b665f8374f8db211a6badb19b27ad5401
PriyanshuCK <priyanshu.sharma.rampura@gmail.com>
2023-01-28 13:26:00 +0530

    Added GTagManager CSP

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withPWA = require('next-pwa')
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app priyanshuck.substack.com substack.com substackapi.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app priyanshuck.substack.com substack.com substackapi.com googletagmanager.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
